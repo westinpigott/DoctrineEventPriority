49ab9242b163878e92d0069319c549f1fdc1e895
Westin Pigott <westin.pigott@techdata.com>
2014-03-27 17:25:48 -0400

    Fixing missing use statements

--- TDMDoctrineEventPriorityBundle.php
+++ TDMDoctrineEventPriorityBundle.php
@@ -3,6 +3,8 @@
 namespace TDM\DoctrineEventPriorityBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Compiler\PassConfig;
 use TDM\DoctrineEventPriorityBundle\DependencyInjection\CompilerPass\OverrideServiceCompilerPass;
 
 class TDMDoctrineEventPriorityBundle extends Bundle {
